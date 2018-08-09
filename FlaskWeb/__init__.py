ef9549ebcdad8044f6341ab4220dceb52b25691a
minjiacheng <37255557+minjiacheng@users.noreply.github.com>
2018-08-09 14:36:48 +0100

    Update __init__.py

--- FlaskWeb/__init__.py
+++ FlaskWeb/__init__.py
@@ -2,17 +2,17 @@
 The flask application package.
 """
 from flask import Flask
-from keras.applications import xception
+#from keras.applications import xception
 #from keras.applications import inception_v3
-import pickle
+#import pickle
 
 #extract bottleneck
 #inception_bottleneck = inception_v3.InceptionV3(weights='imagenet', include_top=False, pooling='avg')
-xception_bottleneck = xception.Xception(weights='imagenet', include_top=False, pooling='avg')
+#xception_bottleneck = xception.Xception(weights='imagenet', include_top=False, pooling='avg')
 #can't load both models onto azure at same time
 #make do with just one
 #load model
-logreg = pickle.load(open('./FlaskWeb/models/logreg_model.sav', 'rb'))
+#logreg = pickle.load(open('./FlaskWeb/models/logreg_model.sav', 'rb'))
 
 UPLOAD_FOLDER = './FlaskWeb/uploads'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg']) #permitted file extensions for user upload
